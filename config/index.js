4d5dc24f405ac706a4a277b521ee5511f6aed606
Nemo Rober <nemo.rober@gmail.com>
2021-06-17 15:50:56 +0200

    Config fix 2

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:4000"
-  : "https://next-tailwind-boilerplate.vercel.app/";
+  : "https://next-tailwind-boilerplate.vercel.app";
